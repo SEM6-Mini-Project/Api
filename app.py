e551e8f4d3c8e42ef6fc4994519e87fc3cd990c3
mayankraj5 <5rajmayank@gmail.com>
2023-04-04 17:08:44 +0530

    API for Mobile Integration

--- app.py
+++ app.py
@@ -12,13 +12,25 @@ def Home():
     return render_template("index.html")
 
 
-@flask_app.route("/predict", methods=["POST"])
-def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
-    return render_template("index.html", prediction_text="The Company Bankruptcy prediction is {}".format(prediction))
+# @flask_app.route("/predict", methods=["POST"])
+# def predict():
+#     float_features = [float(x) for x in request.form.values()]
+#     features = [np.array(float_features)]
+#     prediction = model.predict(features)
+#     return render_template("index.html", prediction_text="The Company Bankruptcy prediction is {}".format(prediction))
 
+@flask_app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    input_data = [data['input_1'], data['input_2'], data['input_3'], data['input_4'], data['input_5'], data['input_6'], data['input_7'], data['input_8'], data['input_9'], data['input_10'], data['input_11'], data['input_12'], data['input_13'], data['input_14'], data['input_15'], data['input_16'], data['input_17'], data['input_18']]
+    prediction = model.predict([input_data])
+    print(prediction)
+    if prediction==[0]:
+        prediction_text="The Company Bankruptcy prediction is not bankrupt"
+    else:
+        prediction_text="The Company Bankruptcy prediction is bankrupt"
+    response = {'prediction': prediction_text}
+    return jsonify(response)
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
