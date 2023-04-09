ac1e653aed798ede9d1867e6d43104315bb3c273
mayankraj5 <5rajmayank@gmail.com>
2023-04-09 14:51:30 +0530

    API Changes

--- app.py
+++ app.py
@@ -10,24 +10,38 @@ model = pickle.load(open("model.pkl", "rb"))
 def Home():
     return render_template("index.html")
 
+@flask_app.route('/predict', methods=['POST'])
 # def predict():
 #     float_features = [float(x) for x in request.form.values()]
+#     print("Float features:", float_features)
 #     features = [np.array(float_features)]
+#     print("Features:", features)
 #     prediction = model.predict(features)
+#     print("Prediction:", prediction)
 #     return render_template("index.html", prediction_text="The Company Bankruptcy prediction is {}".format(prediction))
 
-@flask_app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     input_data = [data['input_1'], data['input_2'], data['input_3'], data['input_4'], data['input_5'], data['input_6'], data['input_7'], data['input_8'], data['input_9'], data['input_10'], data['input_11'], data['input_12'], data['input_13'], data['input_14'], data['input_15'], data['input_16'], data['input_17'], data['input_18']]
-    prediction = model.predict([input_data])
-    print(prediction)
-    if prediction==[0]:
-        prediction_text="The Company Bankruptcy prediction is not bankrupt"
+    print("Input data:", input_data)
+
+    float_data = []
+    for item in input_data:
+        float_data.append(float(item))
+    print(float_data)
+    features = [np.array(float_data)]
+    # input_array = np.array(features).reshape(1, -1)
+    # input_array = np.array(input_data).reshape(1, -1)
+    prediction = model.predict(features)
+    print("Prediction:", prediction)
+    if prediction[0]==0:
+        prediction_text="The company will not go Bankrupt"
     else:
-        prediction_text="The Company Bankruptcy prediction is bankrupt"
+        prediction_text="The company has a high chance of going bankrupt"
     response = {'prediction': prediction_text}
+    print("Response:", response)
     return jsonify(response)
+
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
